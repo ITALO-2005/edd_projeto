a0a4d7b70ce99d199824ab6e833dbaf594d33138
ITALO-2005 <italo-santos.is@academico.ifpb.edu.br>
2026-01-12 18:36:59 +0000

    ..

--- priorityQueue.js
+++ priorityQueue.js
@@ -1,104 +1,84 @@
-class PriorityQueue {
-    constructor() {
-        this.heap = [];
-    }
+// Returns index of parent
+function parent(i) { return Math.floor((i - 1) / 2); }
 
-    // Helper Methods
-    getLeftChildIndex(parentIndex) {
-        return 2 * parentIndex + 1;
-    }
-    getRightChildIndex(parentIndex) {
-        return 2 * parentIndex + 2;
-    }
-    getParentIndex(childIndex) {
-        return Math.floor((childIndex - 1) / 2);
-    }
-    hasLeftChild(index) {
-        return this.getLeftChildIndex(index) < this.heap.length;
-    }
-    hasRightChild(index) {
-        return this.getRightChildIndex(index) < this.heap.length;
-    }
-    hasParent(index) {
-        return this.getParentIndex(index) >= 0;
-    }
-    leftChild(index) {
-        return this.heap[this.getLeftChildIndex(index)];
-    }
-    rightChild(index) {
-        return this.heap[this.getRightChildIndex(index)];
-    }
-    parent(index) {
-        return this.heap[this.getParentIndex(index)];
-    }
-    swap(indexOne, indexTwo) {
-        const temp = this.heap[indexOne];
-        this.heap[indexOne] = this.heap[indexTwo];
-        this.heap[indexTwo] = temp;
-    }
-    peek() {
-        if (this.heap.length === 0) {
-            return null;
-        }
-        return this.heap[0];
-    }
+// Returns index of left child
+function leftChild(i) { return 2 * i + 1; }
 
-    // Removing an element will remove the
-    // top element with highest priority then
-    // heapifyDown will be called
-    remove() {
-        if (this.heap.length === 0) {
-            return null;
-        }
-        const item = this.heap[0];
-        this.heap[0] = this.heap[this.heap.length - 1];
-        this.heap.pop();
-        this.heapifyDown();
-        return item;
-    }
+// Returns index of right child
+function rightChild(i) { return 2 * i + 2; }
 
-    add(item) {
-        this.heap.push(item);
-        this.heapifyUp();
-    }
-
-    heapifyUp() {
-        let index = this.heap.length - 1;
-        while (this.hasParent(index) && this.parent(index) < this.heap[index]) {
-            this.swap(this.getParentIndex(index), index);
-            index = this.getParentIndex(index);
-        }
-    }
-
-    heapifyDown() {
-        let index = 0;
-        while (this.hasLeftChild(index)) {
-            let smallerChildIndex = this.getLeftChildIndex(index);
-            if (this.hasRightChild(index) && this.rightChild(index) > this.leftChild(index)) {
-                smallerChildIndex = this.getRightChildIndex(index);
-            }
-            if (this.heap[index] > this.heap[smallerChildIndex]) {
-                break;
-            } else {
-                this.swap(index, smallerChildIndex);
-            }
-            index = smallerChildIndex;
-        }
+// Shift up to maintain max-heap property
+function shiftUp(i, arr) {
+    while (i > 0 && arr[parent(i)] < arr[i]) {
+        [arr[parent(i)], arr[i]] = [arr[i], arr[parent(i)]];
+        i = parent(i);
     }
 }
 
-// Creating The Priority Queue
-let PriQueue = new PriorityQueue();
-PriQueue.add(32);
-PriQueue.add(45);
-PriQueue.add(12);
-PriQueue.add(65);
-PriQueue.add(85);
+// Shift down to maintain max-heap property
+function shiftDown(i, arr, size) {
+    let maxIndex = i;
+    const l = leftChild(i);
+    if (l < size && arr[l] > arr[maxIndex]) maxIndex = l;
+    const r = rightChild(i);
+    if (r < size && arr[r] > arr[maxIndex]) maxIndex = r;
 
-// Removing and Checking elements of highest Priority
-console.log(PriQueue.peek());
-console.log(PriQueue.remove());
-console.log(PriQueue.peek());
-console.log(PriQueue.remove());
-console.log(PriQueue.peek());
-console.log(PriQueue.remove());
+    if (i !== maxIndex) {
+        [arr[i], arr[maxIndex]] = [arr[maxIndex], arr[i]];
+        shiftDown(maxIndex, arr, size);
+    }
+}
+
+// Insert a new element
+function insert(p, arr) {
+    arr.push(p);
+    shiftUp(arr.length - 1, arr);
+}
+
+// Extract element with maximum priority
+function pop(arr) {
+    const size = arr.length;
+    if (size === 0) return -1;
+    const result = arr[0];
+    arr[0] = arr[size - 1];
+    arr.pop();
+    shiftDown(0, arr, arr.length);
+    return result;
+}
+
+// Get current maximum element
+function getMax(arr) {
+    if (arr.length === 0) return -1;
+    return arr[0];
+}
+
+
+
+// Print heap
+function printHeap(arr) {
+    console.log(arr.join(" "));
+}
+
+// Driver code
+const pq = [];
+
+insert(45, pq);
+insert(20, pq);
+insert(14, pq);
+insert(12, pq);
+insert(31, pq);
+insert(7, pq);
+insert(11, pq);
+insert(13, pq);
+insert(7, pq);
+
+console.log("Priority Queue after inserts: ");
+printHeap(pq);
+
+// Demonstrate getMax
+console.log("Current max element:", getMax(pq));
+
+// Demonstrate extractMax
+pop(pq);
+console.log("Priority Queue after extracting max: ");
+printHeap(pq);
